1661d84fc18887eed7f8f4eee7b588e3b32945af
Kran <56488228+kran27@users.noreply.github.com>
2022-10-30 00:46:07 -0700

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits Altui.forms.darkForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.DarkButton1 = New AltUI.Controls.DarkButton()
+        Me.DarkGroupBox2 = New AltUI.Controls.DarkGroupBox()
+        Me.DarkCheckBox1 = New AltUI.Controls.DarkCheckBox()
+        Me.DarkButton3 = New AltUI.Controls.DarkButton()
+        Me.DarkButton2 = New AltUI.Controls.DarkButton()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DarkTextBox3 = New AltUI.Controls.DarkTextBox()
+        Me.DarkTextBox2 = New AltUI.Controls.DarkTextBox()
+        Me.DarkTextBox1 = New AltUI.Controls.DarkTextBox()
+        Me.DarkGroupBox1 = New AltUI.Controls.DarkGroupBox()
+        Me.DarkGroupBox2.SuspendLayout()
+        Me.DarkGroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DarkButton1
+        '
+        Me.DarkButton1.FlatBottom = False
+        Me.DarkButton1.FlatTop = False
+        Me.DarkButton1.HoldColour = False
+        Me.DarkButton1.Location = New System.Drawing.Point(12, 12)
+        Me.DarkButton1.Name = "DarkButton1"
+        Me.DarkButton1.Padding = New System.Windows.Forms.Padding(5)
+        Me.DarkButton1.Size = New System.Drawing.Size(75, 23)
+        Me.DarkButton1.TabIndex = 0
+        Me.DarkButton1.Text = "Load File"
+        '
+        'DarkGroupBox2
+        '
+        Me.DarkGroupBox2.Controls.Add(Me.DarkCheckBox1)
+        Me.DarkGroupBox2.Controls.Add(Me.DarkButton3)
+        Me.DarkGroupBox2.Controls.Add(Me.DarkButton2)
+        Me.DarkGroupBox2.Controls.Add(Me.Button1)
+        Me.DarkGroupBox2.Controls.Add(Me.DarkTextBox3)
+        Me.DarkGroupBox2.Controls.Add(Me.DarkTextBox2)
+        Me.DarkGroupBox2.Controls.Add(Me.DarkTextBox1)
+        Me.DarkGroupBox2.Location = New System.Drawing.Point(6, 22)
+        Me.DarkGroupBox2.Name = "DarkGroupBox2"
+        Me.DarkGroupBox2.Size = New System.Drawing.Size(277, 168)
+        Me.DarkGroupBox2.TabIndex = 1
+        Me.DarkGroupBox2.TabStop = False
+        Me.DarkGroupBox2.Text = "EMAP"
+        '
+        'DarkCheckBox1
+        '
+        Me.DarkCheckBox1.AutoSize = True
+        Me.DarkCheckBox1.Location = New System.Drawing.Point(164, 26)
+        Me.DarkCheckBox1.Name = "DarkCheckBox1"
+        Me.DarkCheckBox1.Offset = 1
+        Me.DarkCheckBox1.Size = New System.Drawing.Size(107, 19)
+        Me.DarkCheckBox1.TabIndex = 11
+        Me.DarkCheckBox1.Text = "Ignore Lighting"
+        '
+        'DarkButton3
+        '
+        Me.DarkButton3.FlatBottom = False
+        Me.DarkButton3.FlatTop = False
+        Me.DarkButton3.HoldColour = False
+        Me.DarkButton3.Location = New System.Drawing.Point(196, 139)
+        Me.DarkButton3.Name = "DarkButton3"
+        Me.DarkButton3.Padding = New System.Windows.Forms.Padding(5)
+        Me.DarkButton3.Size = New System.Drawing.Size(75, 23)
+        Me.DarkButton3.TabIndex = 10
+        Me.DarkButton3.Text = "Save"
+        '
+        'DarkButton2
+        '
+        Me.DarkButton2.FlatBottom = False
+        Me.DarkButton2.FlatTop = False
+        Me.DarkButton2.HoldColour = False
+        Me.DarkButton2.Location = New System.Drawing.Point(6, 22)
+        Me.DarkButton2.Name = "DarkButton2"
+        Me.DarkButton2.Padding = New System.Windows.Forms.Padding(5)
+        Me.DarkButton2.Size = New System.Drawing.Size(122, 24)
+        Me.DarkButton2.TabIndex = 9
+        Me.DarkButton2.Text = "Set Lighting Colour"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(134, 22)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(24, 24)
+        Me.Button1.TabIndex = 8
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'DarkTextBox3
+        '
+        Me.DarkTextBox3.BackColor = System.Drawing.Color.FromArgb(CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer), CType(CType(29, Byte), Integer))
+        Me.DarkTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.DarkTextBox3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(213, Byte), Integer), CType(CType(213, Byte), Integer), CType(CType(213, Byte), Integer))
+        Me.DarkTextBox3.Location = New System.Drawing.Point(6, 110)
+        Me.DarkTextBox3.Name = "DarkTextBox3"
+        Me.DarkTextBox3.Size = New System.Drawing.Size(265, 23)
+        Me.DarkTextBox3.TabIndex = 2
+        '
+        'DarkTextBox2
+        '
+        Me.DarkTextBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer), CType(CType(29, Byte), Integer))
+        Me.DarkTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.DarkTextBox2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(213, Byte), Integer), CType(CType(213, Byte), Integer), CType(CType(213, Byte), Integer))
+        Me.DarkTextBox2.Location = New System.Drawing.Point(6, 81)
+        Me.DarkTextBox2.Name = "DarkTextBox2"
+        Me.DarkTextBox2.Size = New System.Drawing.Size(265, 23)
+        Me.DarkTextBox2.TabIndex = 1
+        '
+        'DarkTextBox1
+        '
+        Me.DarkTextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer), CType(CType(29, Byte), Integer))
+        Me.DarkTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.DarkTextBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(213, Byte), Integer), CType(CType(213, Byte), Integer), CType(CType(213, Byte), Integer))
+        Me.DarkTextBox1.Location = New System.Drawing.Point(6, 52)
+        Me.DarkTextBox1.Name = "DarkTextBox1"
+        Me.DarkTextBox1.Size = New System.Drawing.Size(265, 23)
+        Me.DarkTextBox1.TabIndex = 0
+        '
+        'DarkGroupBox1
+        '
+        Me.DarkGroupBox1.Controls.Add(Me.DarkGroupBox2)
+        Me.DarkGroupBox1.Location = New System.Drawing.Point(12, 41)
+        Me.DarkGroupBox1.Name = "DarkGroupBox1"
+        Me.DarkGroupBox1.Size = New System.Drawing.Size(776, 401)
+        Me.DarkGroupBox1.TabIndex = 2
+        Me.DarkGroupBox1.TabStop = False
+        Me.DarkGroupBox1.Text = ".map Editor"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 454)
+        Me.Controls.Add(Me.DarkGroupBox1)
+        Me.Controls.Add(Me.DarkButton1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.FromArgb(CType(CType(31, Byte), Integer), CType(CType(31, Byte), Integer), CType(CType(32, Byte), Integer))
+        Me.DarkGroupBox2.ResumeLayout(False)
+        Me.DarkGroupBox2.PerformLayout()
+        Me.DarkGroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DarkButton1 As AltUI.Controls.DarkButton
+    Friend WithEvents DarkGroupBox2 As AltUI.Controls.DarkGroupBox
+    Friend WithEvents DarkTextBox3 As AltUI.Controls.DarkTextBox
+    Friend WithEvents DarkTextBox2 As AltUI.Controls.DarkTextBox
+    Friend WithEvents DarkTextBox1 As AltUI.Controls.DarkTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DarkButton2 As AltUI.Controls.DarkButton
+    Friend WithEvents DarkButton3 As AltUI.Controls.DarkButton
+    Friend WithEvents DarkCheckBox1 As AltUI.Controls.DarkCheckBox
+    Friend WithEvents DarkGroupBox1 As AltUI.Controls.DarkGroupBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,203 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Text
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar
+Imports AltUI.Controls
+
+Public Class Form1
+    Private f As String
+    Private ext As String
+    Private m As Map
+    Private Sub DarkButton1_Click(sender As Object, e As EventArgs) Handles DarkButton1.Click
+        Dim ofd As New OpenFileDialog With {.Filter = "Van Buren Data File|*.AMO;*.ARM;*.CON;*.CRT;*.DOR;*.INT;*.ITM;*.MAP;*.USE;*.VEG;*.WEA", .Multiselect = False, .ValidateNames = True}
+        If ofd.ShowDialog = DialogResult.OK Then
+            f = ofd.FileName
+            ext = f.Substring(f.LastIndexOf("."), 4)
+            Select Case ext
+                Case ".amo"
+                    MsgBox("Not yet implemented")
+                Case ".arm"
+                    MsgBox("Not yet implemented")
+                Case ".con"
+                    MsgBox("Not yet implemented")
+                Case ".crt"
+                    MsgBox("Not yet implemented")
+                Case ".dor"
+                    MsgBox("Not yet implemented")
+                Case ".int"
+                    MsgBox("Not yet implemented")
+                Case ".itm"
+                    MsgBox("Not yet implemented")
+                Case ".map"
+                    m.EMAP = GetRegions("EMAP")(0).ToEMAPc
+                    m.EME2 = GetRegions("EME2")
+                    'm.EMEP = GetRegions("EMEP")
+                    'm.ECAM = GetRegions("ECAM")
+                    'm.EMNP = GetRegions("EMNP")
+                    'm.EMEF = GetRegions("EMEF")
+                    Button1.BackColor = m.EMAP.col : Button1.FlatAppearance.MouseOverBackColor = col : Button1.FlatAppearance.MouseDownBackColor = col
+                    DarkTextBox1.Text = m.EMAP.s1
+                    DarkTextBox2.Text = m.EMAP.s2
+                    DarkTextBox3.Text = m.EMAP.s3
+                    DarkCheckBox1.Checked = m.EMAP.il
+                Case ".use"
+                    MsgBox("Not yet implemented")
+                Case ".veg"
+                    MsgBox("Not yet implemented")
+                Case ".wea"
+                    MsgBox("Not yet implemented")
+            End Select
+        End If
+    End Sub
+
+    Private Sub DarkButton2_Click(sender As Object, e As EventArgs) Handles DarkButton2.Click
+        Dim cd As New ColorDialog With {.Color = m.EMAP.col, .FullOpen = True}
+        If cd.ShowDialog() = DialogResult.OK Then
+            Button1.BackColor = cd.Color : Button1.FlatAppearance.MouseDownBackColor = cd.Color : Button1.FlatAppearance.MouseOverBackColor = cd.Color
+            m.EMAP.col = cd.Color
+        End If
+    End Sub
+
+    Private Sub DarkButton3_Click(sender As Object, e As EventArgs) Handles DarkButton3.Click
+        m.EMAP.s1 = DarkTextBox1.Text
+        m.EMAP.s2 = DarkTextBox2.Text
+        m.EMAP.s3 = DarkTextBox3.Text
+        m.EMAP.il = DarkCheckBox1.Checked
+        Dim t As New List(Of Byte)
+        t.AddRange(m.EMAP.ToEMAPb)
+        For Each b In m.EME2.ToEME2b
+            t.AddRange(b)
+        Next
+        '    t.AddRange(m.EMEP.ToEMEPb)
+        '    t.AddRange(m.ECAM.ToECAMb)
+        '    t.AddRange(m.EMNP.ToEMNPb)
+        '    t.AddRange(m.EMEF.ToEMEFb)
+        IO.File.WriteAllBytes("out.map", t.ToArray)
+    End Sub
+End Class
+Public Class Map
+    Property EMAP As EMAPc
+    Property EME2 As EME2c()
+    Property EMEP As EMEPc
+    Property ECAM As ECAMc
+    Property EMNP As EMNPc
+    Property EMEF As EMEFc
+End Class
+Public Class EMAPc
+    Property s1 As String
+    Property s2 As String
+    Property s3 As String
+    Property col As Color
+    Property il As Boolean
+    Property le As Integer
+End Class
+Public Class EME2c
+
+    Property inv As String()
+End Class
+Public Class EMEPc
+
+End Class
+Public Class ECAMc
+
+End Class
+Public Class EMNPc
+
+    End Class
+    Public Class EMEFc
+
+    End Class
+    Friend Module Functions
+    <System.Runtime.CompilerServices.Extension>
+    Public Function ToEMAPc(b As Byte()) As EMAPc
+        Dim s1o = 16 + 2 : Dim s1l = b(s1o - 2)
+        Dim s2o = s1o + s1l + 2 : Dim s2l = b(s2o - 2)
+        Dim s3o = s2o + s2l + 2 : Dim s3l = b(s3o - 2)
+        Return New EMAPc With {
+            .s1 = Encoding.ASCII.GetString(b.Skip(s1o).Take(s1l).ToArray()),
+            .s2 = Encoding.ASCII.GetString(b.Skip(s2o).Take(s2l).ToArray()),
+            .s3 = Encoding.ASCII.GetString(b.Skip(s3o).Take(s3l).ToArray()),
+            .col = Color.FromArgb(b(s3o + s3l + 2), b(s3o + s3l + 3), b(s3o + s3l + 4)),
+            .il = b(4) = 0,
+            .le = b(s3o + s3l)
+        }
+    End Function
+    <System.Runtime.CompilerServices.Extension>
+    Public Function ToEMAPb(c As EMAPc) As Byte()
+        Dim s = 49 + c.s1.Length + c.s2.Length + c.s3.Length
+        Dim out = New Byte(s - 1) {}
+        out.OverwriteBytes(0, Encoding.ASCII.GetBytes("EMAP"))
+        out.OverwriteBytes(4, New Byte() {If(c.il, 0, 5)})
+        out.OverwriteBytes(8, New Byte() {s})
+        out.OverwriteBytes(16, New Byte() {c.s1.Length})
+        out.OverwriteBytes(18, Encoding.ASCII.GetBytes(c.s1))
+        out.OverwriteBytes(18 + c.s1.Length, New Byte() {c.s2.Length})
+        out.OverwriteBytes(20 + c.s1.Length, Encoding.ASCII.GetBytes(c.s2))
+        out.OverwriteBytes(20 + c.s1.Length + c.s2.Length, New Byte() {c.s3.Length})
+        out.OverwriteBytes(22 + c.s1.Length + c.s2.Length, Encoding.ASCII.GetBytes(c.s3))
+        out.OverwriteBytes(22 + c.s1.Length + c.s2.Length + c.s3.Length, New Byte() {c.le})
+        out.OverwriteBytes(24 + c.s1.Length + c.s2.Length + c.s3.Length, New Byte() {c.col.R})
+        out.OverwriteBytes(25 + c.s1.Length + c.s2.Length + c.s3.Length, New Byte() {c.col.G})
+        out.OverwriteBytes(26 + c.s1.Length + c.s2.Length + c.s3.Length, New Byte() {c.col.B})
+        IO.File.WriteAllBytes("out.bin", out)
+        Return out
+    End Function
+    Private ReadOnly Empty(-1) As Integer
+
+    <System.Runtime.CompilerServices.Extension>
+    Public Function Locate(ByVal self() As Byte, ByVal candidate() As Byte) As Integer()
+        If IsEmptyLocate(self, candidate) Then
+            Return Empty
+        End If
+
+        Dim list As New List(Of Integer)()
+
+        Dim i As Integer = 0
+        Do While i < self.Length
+            If Not IsMatch(self, i, candidate) Then
+                i += 1
+                Continue Do
+            End If
+
+            list.Add(i)
+            i += 1
+        Loop
+
+        Return If(list.Count = 0, Empty, list.ToArray())
+    End Function
+    Private Function IsMatch(ByVal array() As Byte, ByVal position As Integer, ByVal candidate() As Byte) As Boolean
+        If candidate.Length > (array.Length - position) Then
+            Return False
+        End If
+
+        For i As Integer = 0 To candidate.Length - 1
+            If array(position + i) <> candidate(i) Then
+                Return False
+            End If
+        Next i
+
+        Return True
+    End Function
+    Private Function IsEmptyLocate(ByVal array() As Byte, ByVal candidate() As Byte) As Boolean
+        Return array Is Nothing OrElse candidate Is Nothing OrElse array.Length = 0 OrElse candidate.Length = 0 OrElse candidate.Length > array.Length
+    End Function
+    Public Function GetRegions(h1s As String) As List(Of Byte())
+        Dim hl As New List(Of Byte())
+        Dim file = IO.File.ReadAllBytes(name)
+        Dim h1n = Encoding.ASCII.GetBytes(h1s)
+        Dim h1c = file.Locate(h1n)
+        For Each l In h1c
+            Dim tb = file.Skip(l + 8).Take(4).ToArray()
+            Dim tl = BitConverter.ToInt32(tb, 0)
+            Dim h1 = file.Skip(l).Take(tl).ToArray()
+            hl.Add(h1)
+        Next
+        Return hl
+    End Function
+    <System.Runtime.CompilerServices.Extension>
+    Public Sub OverwriteBytes(ByRef b As Byte(), startIndex As Integer, newBytes As Byte())
+        For i = startIndex To startIndex + newBytes.Length - 1
+            b(i) = newBytes(i - startIndex)
+        Next
+    End Sub
+End Module
+
